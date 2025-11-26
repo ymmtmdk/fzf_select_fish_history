f1422b52b546350b344127810a33207dbc1d67e6
yamamoto madoka <ymmtmdk@gmail.com>
2025-11-26 10:50:01 +0900

    m

--- functions/fzf_select_fish_history.fish
+++ functions/fzf_select_fish_history.fish
@@ -4,8 +4,6 @@ function fzf_select_fish_history
     end
 
     # history save
-    #history --show-time="%y/%m/%d %H:%M │ "history | fzf | read cmd
-
     history merge
     history --show-time='%y-%m-%d %H:%M> ' | fzf | sed -e "s/.\{16\}//" | read cmd
 
@@ -13,19 +11,3 @@ function fzf_select_fish_history
         commandline $cmd
     end
 end
-
-#
-#        builtin history --null --show-time="$fzf_history_time_format │ " |
-#        _fzf_wrapper --read0 \
-#            --print0 \
-#            --multi \
-#            --scheme=history \ --prompt="History> " \
-#            --query=(commandline) \
-#            --preview="string replace --regex '$time_prefix_regex' '' -- {} | fish_indent --ansi" \
-#            --preview-window="bottom:3:wrap" \
-#            $fzf_history_opts |
-#        string split0 |
-#        # remove timestamps from commands selected
-#        string replace --regex $time_prefix_regex ''
-#
-bind ctrl-r fzf_select_fish_history
