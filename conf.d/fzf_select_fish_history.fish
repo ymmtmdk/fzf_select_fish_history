2d8b6b4f23fd9715fb1386995bc0458cc62e7aca
yamamoto madoka <ymmtmdk@gmail.com>
2025-11-27 09:53:04 +0900

    m

--- conf.d/fzf_select_fish_history.fish
+++ conf.d/fzf_select_fish_history.fish
@@ -1,15 +1,15 @@
 function fzf_select_fish_history
     if test (count $argv) -gt 0
-        set flags --query "$argv"
+        set qry -q "$argv"
     end
 
     # history save
     history merge
-    history --show-time='%y-%m-%d %H:%M> ' | fzf | sed -e "s/.\{16\}//" | read cmd
+    history --show-time='%y-%m-%d %H:%M> ' | fzf $qry | sed -e "s/.\{16\}//" | read cmd
 
     if [ $cmd ]
         commandline $cmd
     end
 end
 
-bind ctrl-r fzf_select_fish_history
+bind ctrl-r fzf_select_fish_history (commandline)
